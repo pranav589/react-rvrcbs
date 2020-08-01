4be99db9a2a232e773b72b2d758bda43041cc1a6
pranav589 <pranav.bakale@gmail.com>
2020-08-01 13:10:29 +0530

    1 file modified

--- Ninjas.js
+++ Ninjas.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Ninjas extends Component{
-  render(){
-    const { ninjas } = this.props;
+const Ninjas =({ninjas})=>{
+    
+    
     const ninjaList = ninjas.map(ninja => {
       return (
         <div className="ninja" key={ninja.id}>
@@ -17,7 +17,7 @@ class Ninjas extends Component{
         { ninjaList }
       </div>
     )
-  }
+  
 }
 
 export default Ninjas
