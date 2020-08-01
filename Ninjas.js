fbb1883c9d201cff57eb344c46074bab681da52c
pranav589 <pranav.bakale@gmail.com>
2020-08-01 13:26:12 +0530

    1 file modified

--- Ninjas.js
+++ Ninjas.js
@@ -1,22 +1,15 @@
 import React from 'react'
 
 const Ninjas =({ninjas})=>{
-    
-    
-    const ninjaList = ninjas.map(ninja => {
-      if (ninja.age > 26){
-      return (
-        <div className="ninja" key={ninja.id}>
-          <div>Name: { ninja.name }</div>
-          <div>Age: { ninja.age }</div>
-          <div>Belt: { ninja.belt }</div>
-        </div>
-      )
-      }
-      else{
-        return null
-      }
-    });
+    const ninjaList = ninjas.map(ninja=>{
+     return ninja.age > 26 ? (
+       <div className="ninja" key={ninja.id}>
+        <div>Name: { ninja.name }</div>
+        <div>Age: { ninja.age }</div>
+        <div>Belt: { ninja.belt }</div>
+      </div>
+     ) : null;
+    })
     return (
       <div className="ninja-list">
         { ninjaList }
