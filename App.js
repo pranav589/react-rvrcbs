53a41468cba1a81a25ff71188678cbf8f5e43f89
pranav589 <pranav.bakale@gmail.com>
2020-08-02 10:54:52 +0530

    3 files modified

--- App.js
+++ App.js
@@ -10,17 +10,22 @@ class App extends Component {
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    });
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app</h1>
         <Ninjas ninjas={this.state.ninjas}/>
-        <AddNinja/>
+        <AddNinja addNinja={this.addNinja} />
       </div>
     );
   }
 }
 
 export default App;
-
-
